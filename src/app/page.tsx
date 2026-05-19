e37df00023d44b41d29aff559c8fabda8b9da4b9
butterkookies <geronimoandreijohn.pdm@gmail.com>
2026-05-19 11:44:58 +0800

    feat: apply widget glass design + fix Vercel prerender (force-dynamic)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useState, useRef, useCallback } from 'react'
 import { useTasks } from '@/hooks/useTasks'
 import { useKeyboard } from '@/hooks/useKeyboard'
